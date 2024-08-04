543e1780e8fc56c51e350a7cf0a40e460b21eec7
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2024-08-04 12:15:00 -0500

    Basic dropdown picker for layout

--- src/app/components/NoteCreator/index.tsx
+++ src/app/components/NoteCreator/index.tsx
@@ -5,22 +5,35 @@ import { useStore } from '../../hooks/store'
 function NoteCreator() {
   const store = useStore()
   const [title, setTitle] = useState('')
+  const [layout, setLayout] = useState(BlockLayout.NONE)
 
-  function onChange(event: ChangeEvent<HTMLInputElement>) {
+  function onTitleChange(event: ChangeEvent<HTMLInputElement>) {
     setTitle(event.target.value)
+  }
+
+  function onLayoutChange(event: ChangeEvent<HTMLSelectElement>) {
+    setLayout(event.target.value as BlockLayout)
   }
 
   async function onClick() {
     const name = title
     const content = ''
-    const block = Block.create({ name, content, layout: BlockLayout.TASK })
+    const block = Block.create({ name, content, layout })
     await store.command('add', block)
     setTitle('')
+    setLayout(BlockLayout.NONE)
   }
 
   return (
     <div>
-      <input type="text" value={title} onChange={onChange} />
+      <input type="text" value={title} onChange={onTitleChange} />
+
+      <select value={layout} onChange={onLayoutChange}>
+        <option value={BlockLayout.NONE}>None</option>
+        <option value={BlockLayout.TASK}>Task</option>
+        <option value={BlockLayout.NOTE}>Note</option>
+      </select>
+
       <button onClick={onClick}>Add</button>
       <p>{title}</p>
     </div>
